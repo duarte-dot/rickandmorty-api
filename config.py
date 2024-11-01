4b2283f66dc08e8ae6194b34965cb3c2aaa0a830
Gabriel Duarte <gabrieldvr@outlook.com>
2024-11-01 14:15:27 -0300

    adiciona configuração inicial do Flask e SQLAlchemy

---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+class Config:
+    SECRET_KEY = os.getenv('SECRET_KEY', 'chave_secreta')
+
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
